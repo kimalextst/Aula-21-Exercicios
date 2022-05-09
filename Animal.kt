dc2c53dd6fff3acf551f1962e5f5743fac9c1f05
kimalextst <96539589+kimalextst@users.noreply.github.com>
2022-05-09 15:48:09 -0300

    Exercicio 2

---
+++ Animal.kt
@@ -0,0 +1,26 @@
+package exercicio2
+
+open class Animal(
+    val nome: String = "",
+    val raca: String = "",
+    private val pessoaTutora: String
+        ){
+    protected open fun movimentar(): String {
+        return ""
+    }
+
+    protected open fun comer(): String {
+        return ""
+    }
+
+    protected open fun dormir(): String {
+        return ""
+    }
+
+    fun infoAnimal(){
+        println("-----------------------------------------------------------------")
+        println("Nome: $nome")
+        println("Raça: $raca")
+        println("Pessoa tutora: $pessoaTutora")
+    }
+}
